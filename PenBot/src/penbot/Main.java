c98f20b2ad04783264898baafff98272400a7811
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2014-12-29 01:47:36 +0200

    vim <3 gg=G

--- PenBot/src/penbot/Main.java
+++ PenBot/src/penbot/Main.java
@@ -5,39 +5,39 @@ import lejos.nxt.Motor;
 
 /**
  * Main function
- * 
+ *
  */
 public class Main {
 
     public static void main(String[] args) {
-		// physical bot dimensions, in mm
-		double axis = 120.0;		// wheel dist for DifferentialPilot
-		double wheelSize = 56.0;	// wheel size for DifferentialPilot
-		double penDist = 64.0;		// dist of pen tip to axis
-		
-		// physical game board dimensions, also mm
-		double outerCellSize = 14.0;
-		double markSize = 10.0;
-		
-		double botDistToBoard = 50;
-		
-		Board ticpaper = new Board(outerCellSize, penDist);
-		Penbot ironman = new Penbot(ticpaper, axis, wheelSize, penDist, 
-				outerCellSize, markSize, botDistToBoard);
-		
-		System.out.println("I AM IRON MAN");
-		Button.waitForAnyPress();
-		System.out.println("I PRINT");
-		for (int i = 0; i < 3; i++) {
-		    for (int j = 0; j < 3; j++) {
-		        ironman.drawCross(i, j);
-		        if (Button.ESCAPE.isDown()) {
-		            System.out.println("EMERGENCY STOP; HALTING");
-		            return;
-		        }
-		    }
-		}
-		System.out.println("FIN.");
-		Button.waitForAnyPress();
-	}
+        // physical bot dimensions, in mm
+        double axis = 120.0;		// wheel dist for DifferentialPilot
+        double wheelSize = 56.0;	// wheel size for DifferentialPilot
+        double penDist = 64.0;		// dist of pen tip to axis
+
+        // physical game board dimensions, also mm
+        double outerCellSize = 14.0;
+        double markSize = 10.0;
+
+        double botDistToBoard = 50;
+
+        Board ticpaper = new Board(outerCellSize, penDist);
+        Penbot ironman = new Penbot(ticpaper, axis, wheelSize, penDist,
+                outerCellSize, markSize, botDistToBoard);
+
+        System.out.println("I AM IRON MAN");
+        Button.waitForAnyPress();
+        System.out.println("I PRINT");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                ironman.drawCross(i, j);
+                if (Button.ESCAPE.isDown()) {
+                    System.out.println("EMERGENCY STOP; HALTING");
+                    return;
+                }
+            }
+        }
+        System.out.println("FIN.");
+        Button.waitForAnyPress();
+    }
 }
