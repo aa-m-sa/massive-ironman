ceac39e55705e045e20ee378bdd77c98d75bae7a
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2014-12-29 07:28:21 +0200

    a test for calibrator routine

---
+++ PenBot/src/test/PenConfigureTest.java
@@ -0,0 +1,37 @@
+package test;
+import penbot.Board;
+import penbot.Penbot;
+
+/**
+ * Does configuring the pen motor rotation angle work?
+ *
+ * a 'test':
+ * compile with testMain() as main function; upload and run
+ *
+ */
+
+public class PenConfigureTest {
+
+    public static void main() {
+
+        double axis = 120.0;
+        double wheelSize = 56.0;
+        double penDist = 70.0;
+        double outerCellSize = 14.0;
+        double markSize = 10.0;
+        double botDistToBoard = 50;
+
+        Board ticpaper = new Board(outerCellSize, penDist);
+        Penbot ironman = new Penbot(ticpaper, axis, wheelSize, penDist,
+                outerCellSize, markSize, botDistToBoard);
+
+        int cAngle = ironman.calibratePenAngle();
+        System.out.println("Calibrated:");
+        System.out.println(cAngle);
+
+        // test by drawing a X
+        ironman.drawCross(0,0);
+
+    }
+
+}
