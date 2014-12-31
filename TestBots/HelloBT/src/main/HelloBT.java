e2c648b351d52e9219acdc776320f2aba2954727
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2014-12-31 23:47:43 +0200

    starting a more simple testbot for BT

---
+++ TestBots/HelloBT/src/main/HelloBT.java
@@ -0,0 +1,15 @@
+package main;
+
+import lejos.nxt.*;
+
+/**
+ * Simple bluetooth connection tester bot
+ *
+ */
+public class HelloBT {
+
+	public static void main(String[] args) {
+		System.out.println("I AM IRON MAN");
+		Button.waitForAnyPress();
+	}
+}
