30ba5958f466384ec028d7167e9020082cd834bb
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2015-01-02 09:20:59 +0200

    update PenConfigure 'Test'

--- PenBot/src/test/PenConfigureTest.java
+++ PenBot/src/test/PenConfigureTest.java
@@ -14,16 +14,7 @@ public class PenConfigureTest {
 
     public static void main(String[] args) {
 
-        double axis = 120.0;
-        double wheelSize = 56.0;
-        double penDist = 70.0;
-        double outerCellSize = 14.0;
-        double markSize = 10.0;
-        double botDistToBoard = 50;
-
-        Board ticpaper = new Board(outerCellSize, penDist);
-        Penbot ironman = new Penbot(ticpaper, axis, wheelSize, penDist,
-                outerCellSize, markSize, botDistToBoard);
+        Penbot ironman = new Penbot();
 
         int cAngle = ironman.calibratePenAngle();
         System.out.println("Calibrated:");
