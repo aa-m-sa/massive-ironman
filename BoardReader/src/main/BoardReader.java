6068586670636abb03d82287613717544574e1ea
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2015-01-05 13:50:11 +0200

    opencv java hello wörld

--- BoardReader/src/main/BoardReader.java
+++ BoardReader/src/main/BoardReader.java
@@ -31,101 +31,13 @@ import org.opencv.core.Mat;
  * TODO: when able to read the board, combine this project with the Game
  */
 public class BoardReader {
-    private FrameGrabber grabber;
-    private CvMemStorage storage;
-    private int width, height;
-
-    private CanvasFrame frame;
-
-    /**
-     * Initialize BoardReader.
-     *
-     * Creates a FrameGrabber and ...?
-     * */
-    public BoardReader() throws Exception {
-        // from Demo.java:
-        // Preload the opencv_objdetect module to work around a known bug.
-        Loader.load(opencv_objdetect.class);
-
-        // create a default grabber for the webcam device no. 1
-        grabber = FrameGrabber.createDefault(1);
-        grabber.start();
-
-        // storage, if needed (?)
-        storage = CvMemStorage.create();
-
-        // get width and height of the image produced with grabber
-        // (from Demo)
-        IplImage im = grabber.grab();
-        width  = im.width();
-        height = im.height();
-        // ??? try these:
-        //grabber.getImageWidth();
-        //grabber.getImageHeight();
-
-        frame = new CanvasFrame("Tic Tac Toe Board Reader test",
-                CanvasFrame.getDefaultGamma()/grabber.getGamma());
-
-
-    }
-
-    /**
-     * Find the board.
-     *
-     * @return if a board was successfully recognized from a frame given by Grabber.
-     */
-    public boolean findBoard() {
-        // first try finding the bounding box
-        try {
-            IplImage image = grabber.grab();
-        } catch(FrameGrabber.Exception e){
-            System.out.println("FrameGrabber exception; couldn't find a board");
-            e.printStackTrace();
-            return false;
-        }
-        return false;
-    }
-
-    // ??
-    public void run() throws FrameGrabber.Exception {
-        // loop: show grayed image on the canvas
-        IplImage grayImage  = IplImage.create(width, height, IPL_DEPTH_8U, 1);
-        IplImage grabbedImage;
-
-        while (frame.isVisible() && (grabbedImage = grabber.grab()) != null) {
-            cvClearMemStorage(storage);
-
-            // easier to work with grayscale images
-            cvCvtColor(grabbedImage, grayImage, CV_BGR2GRAY);
-
-            frame.showImage(grayImage);
-        }
-    }
-
-    /**
-     * Dispose frame, stop grabber etc.
-     */
-    public void closeReader() {
-        // quit
-        frame.dispose();
-        try {
-            grabber.stop();
-        } catch (FrameGrabber.Exception e) {
-            System.out.println("Couldn't stop FrameGrabber!");
-            e.printStackTrace();
-        }
-    }
+    
 
     public static void main(String[] args) throws Exception {
+    	// opencv hello world
+    	System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+        Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
+        System.out.println( "mat = " + mat.dump() );
 
-        System.out.print("trying to construct reader...");
-        BoardReader reader = new BoardReader();
-        System.out.println("ok!");
-        System.out.print("trying to find a board...");
-        boolean boardFound = reader.findBoard();
-        System.out.println("finished, found:" + boardFound);
-        //reader.run();
-        System.out.println("closing...");
-        reader.closeReader();
     }
 }
