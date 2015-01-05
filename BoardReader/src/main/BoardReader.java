3250564bdf9ec015860aa9d19995fcbc14036748
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2015-01-05 15:47:35 +0200

    test image load + write

--- BoardReader/src/main/BoardReader.java
+++ BoardReader/src/main/BoardReader.java
@@ -1,8 +1,11 @@
 package main;
 
 import org.opencv.core.Core;
+import org.opencv.core.Size;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
 
 /**
  * The actual attempt at Tic Tac Toe Board reading.
@@ -31,13 +34,56 @@ import org.opencv.core.Mat;
  * TODO: when able to read the board, combine this project with the Game
  */
 public class BoardReader {
-    
+
+    public BoardReader() {
+        // constructor
+    }
+
+    /**
+     * Recognize the tic tac toe board in webcam video (TODO, now use test jpg picture)
+     *
+     * @return was the board found successfully
+     */
+    public boolean findBoard() {
+        // test using the a still image
+        // ???
+        //System.out.println(getClass().getResource("/resources/test1.jpg"));
+        // ^ oikea polku tiedostoon, printtaus onnistuu
+        // mutta
+        //Mat image = Highgui.imread(getClass().getResource("/resources/test1.jpg").getPath());
+        // luo tyhjän imagen
+        // sen sijaan
+        Mat image = Highgui.imread("bin/resources/test1.jpg");  // toimii
+        System.out.println(image.channels());
+
+        // grayscale image will have just one channel
+        //Mat grayImage = new Mat(image.height(), image.width(), CvType.CV_8UC1);
+        if (image.empty()) {
+            System.out.println("error");
+            return false;
+        }
+        Mat grayImage = new Mat();
+        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
+
+        // blur the image with GaussianBlur to get rid of noise etc
+        Imgproc.GaussianBlur(grayImage, grayImage, new Size(21, 21), 0);
+
+        System.out.println("writing blurred grayscale..");
+        Highgui.imwrite("test_blur_gray.jpg", grayImage);
+
+        return false;
+    }
 
     public static void main(String[] args) throws Exception {
-    	// opencv hello world
+    	// load native library
+        System.out.println("Load OpenCV native libs...");
     	System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-        Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
-        System.out.println( "mat = " + mat.dump() );
+        System.out.println("ok!");
+
+        System.out.println("try loading test image");
+        BoardReader breader = new BoardReader();
+        breader.findBoard();
+        System.out.println("ok!");
 
     }
 }
